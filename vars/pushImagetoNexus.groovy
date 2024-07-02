d532f3c9b750bb1a89f71ce17920a26ec944b274
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-07-02 13:51:49 +0300

    add parameters

--- vars/pushImagetoNexus.groovy
+++ vars/pushImagetoNexus.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String imageName, String Version){
+def call(String imageName, String Versionersion){
     withCredentials([usernamePassword(credentialsId: 'Nexus_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin 192.168.206.129:8083"
-        sh "docker tag $imageName 192.168.206.129:8081/repository/$imageName:$Version"
-        sh "docker push 192.168.206.129:8083/repository/$imageName:$Version"
+        sh "docker tag $imageName 192.168.206.129:8081/repository/$imageName:$version"
+        sh "docker push 192.168.206.129:8083/repository/$imageName:$version"
     }
 }
 
