530db87f429f5532ec3a77a232c1035e8eb18588
AhmedGaberElbltagy <ahmedelbltagy1999@gmail.com>
2024-07-02 13:45:10 +0300

    add parameters

--- vars/pushImagetoNexus.groovy
+++ vars/pushImagetoNexus.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName, String Version){
     withCredentials([usernamePassword(credentialsId: 'Nexus_Credientials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin 192.168.206.129:8083"
-        sh 'docker tag java-maven-app 192.168.206.129:8081/repository/java-maven-app'
-        sh 'docker push 192.168.206.129:8083/repository/java-maven-app'
+        sh "docker tag ${imageName} 192.168.206.129:8081/repository/${imageName}:${Version}"
+        sh "docker push 192.168.206.129:8083/repository/${imageName}:${Version}"
     }
 }
+
+#java-maven-app
